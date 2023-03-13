7a5db90624dd38cb10b6cc3cfaef3a2003689332
LucSciammetta <75546252+LucSciammetta@users.noreply.github.com>
2023-03-13 16:29:54 -0400

    Added more pins to code

--- testdriver/testdriver.ino
+++ testdriver/testdriver.ino
@@ -23,10 +23,19 @@
 const int SLAVE_ADDRESS = 100;
 
 // define the pins we'll use for our various pushbuttons
-const int PUSHBUTTON1 = 7;
-const int PUSHBUTTON2 = 8;
-const int PUSHBUTTON3 = 9;
-const int PUSHBUTTON4 = 10;
+const int PUSHBUTTON1 = 2;
+const int PUSHBUTTON2 = 3;
+const int PUSHBUTTON3 = 4;
+const int PUSHBUTTON4 = 5;
+const int PUSHBUTTON5 = 6;
+const int PUSHBUTTON6 = 7;
+const int PUSHBUTTON7 = 8;
+const int PUSHBUTTON8 = 9;
+// const int PUSHBUTTON9 = 10;
+// const int PUSHBUTTON10 = 11;
+// const int PUSHBUTTON11 = 12;
+// const int PUSHBUTTON12 = 13;
+
 
 // define the commands we'll send down the wire
 // the master and slave need to agree on what these are!
@@ -34,6 +43,15 @@ const char COMMAND1 = 1;
 const char COMMAND2 = 2;
 const char COMMAND3 = 3;
 const char COMMAND4 = 4;
+const char COMMAND5 = 5;
+const char COMMAND6 = 6;
+const char COMMAND7 = 7;
+const char COMMAND8 = 8;
+// const char COMMAND9 = 9;
+// const char COMMAND10 = 10;
+// const char COMMAND11 = 11;
+// const char COMMAND12 = 12;
+
 
 
 void setup() {
@@ -44,6 +62,15 @@ void setup() {
   pinMode(PUSHBUTTON2, INPUT_PULLUP);
   pinMode(PUSHBUTTON3, INPUT_PULLUP);
   pinMode(PUSHBUTTON4, INPUT_PULLUP);
+  pinMode(PUSHBUTTON5, INPUT_PULLUP);
+  pinMode(PUSHBUTTON6, INPUT_PULLUP);
+  pinMode(PUSHBUTTON7, INPUT_PULLUP);
+  pinMode(PUSHBUTTON8, INPUT_PULLUP);
+  // pinMode(PUSHBUTTON9, INPUT_PULLUP);
+  // pinMode(PUSHBUTTON10, INPUT_PULLUP);
+  // pinMode(PUSHBUTTON11, INPUT_PULLUP);
+  // pinMode(PUSHBUTTON12, INPUT_PULLUP);
+
 
   //Serial.begin(9600);           // start serial for debugging output
   //Serial.println("Test Driver Ready...");
@@ -68,7 +95,39 @@ void loop() {
     sendCommand(COMMAND4);
     while (digitalRead(PUSHBUTTON4) == LOW) {      
     }
-  }
+  } else if (digitalRead(PUSHBUTTON5) == LOW) {
+    sendCommand(COMMAND5);
+    while (digitalRead(PUSHBUTTON5) == LOW){
+    }
+  }else if (digitalRead(PUSHBUTTON6) == LOW) {
+    sendCommand(COMMAND6);
+    while (digitalRead(PUSHBUTTON6) == LOW){
+    }
+  }else if (digitalRead(PUSHBUTTON7) == LOW) {
+    sendCommand(COMMAND7);
+    while (digitalRead(PUSHBUTTON7) == LOW){
+    }
+  }else if (digitalRead(PUSHBUTTON8) == LOW) {
+    sendCommand(COMMAND8);
+    while (digitalRead(PUSHBUTTON8) == LOW){
+    }
+  }//else if (digitalRead(PUSHBUTTON9) == LOW) {
+  //   sendCommand(COMMAND9);
+  //   while (digitalRead(PUSHBUTTON9) == LOW){
+  //   }
+  // }else if (digitalRead(PUSHBUTTON10) == LOW) {
+  //   sendCommand(COMMAND10);
+  //   while (digitalRead(PUSHBUTTON10) == LOW){
+  //   }
+  // }else if (digitalRead(PUSHBUTTON11) == LOW) {
+  //   sendCommand(COMMAND11);
+  //   while (digitalRead(PUSHBUTTON11) == LOW){
+  //   }
+  // }else if (digitalRead(PUSHBUTTON12) == LOW) {
+  //   sendCommand(COMMAND12);
+  //   while (digitalRead(PUSHBUTTON12) == LOW){
+  //   }
+  // }
 };
 
 void sendCommand(int command) {
